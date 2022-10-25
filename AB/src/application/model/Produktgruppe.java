80f41dc2d1c1b44fd40098b0de837bc580777688
Studsk <kvejborgrasmus@gmail.com>
2022-10-25 10:35:42 +0200

    Har lavet enkeltrettet aggregering mellem Produktgruppe og Produkt

--- AB/src/application/model/Produktgruppe.java
+++ AB/src/application/model/Produktgruppe.java
@@ -1,4 +1,35 @@
 package application.model;
 
+import java.util.ArrayList;
+
 public class Produktgruppe {
+
+
+
+    private final ArrayList<Produkt> produkter = new ArrayList<>();
+
+    public ArrayList<Produkt> getProdukter(){
+        return new ArrayList<>(produkter);
+    }
+
+    public Produkt createProdukt(String produktNavn){
+        Produkt produkt = new Produkt(produktNavn);
+        produkter.add(produkt);
+        return produkt;
+    }
+
+    public void addProdukt(Produkt produkt){
+        if (!produkter.contains(produkt)){
+            produkter.add(produkt);
+        }
+    }
+
+    public void removeProdukt(Produkt produkt){
+        if (produkter.contains(produkt)){
+            produkter.remove(produkt);
+        }
+    }
+
+
+
 }
