16f889aeafc309e22d4a3b6ad50b0cf14b8451de
Lucas <eaaluob@students.eaaa.dk>
2022-11-01 10:32:49 +0100

    Oprettet gui til Rundvisning

--- AB/src/GUI/OpretKundeWindow.java
+++ AB/src/GUI/OpretKundeWindow.java
@@ -4,6 +4,7 @@ import application.Controller.Controller;
 import application.model.Kunde;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
@@ -15,12 +16,14 @@ public class OpretKundeWindow extends Stage {
     private TextField txfNavn, txfEmail, txfTelefon, txfAdresse;
     private Label lblNavn, lblEmail, lblTelefon, lblAdresse;
     private Label lblError;
+    private Button btnOpretKunde;
 
     public OpretKundeWindow(String opretKunde){
         this.initStyle(StageStyle.UTILITY);
         this.initModality(Modality.APPLICATION_MODAL);
         this.setResizable(false);
 
+        this.setTitle("Opret kunde");
         GridPane pane = new GridPane();
         this.initContent(pane);
 
@@ -34,13 +37,32 @@ public class OpretKundeWindow extends Stage {
         pane.setHgap(20);
         pane.setVgap(10);
 
-
+        lblNavn = new Label("Navn:");
         txfNavn = new TextField();
-       pane.add(txfNavn,1,1);
-//        pane.add(txfTelefon,1,2);
-//        pane.add(txfEmail,1,3);
-//        pane.add(txfAdresse,1,4);
+        pane.add(lblNavn,0,1);
+        pane.add(txfNavn,1,1);
 
+        lblTelefon = new Label("Telefon:");
+        txfTelefon = new TextField();
+        pane.add(lblTelefon,0,2);
+        pane.add(txfTelefon,1,2);
+
+        lblEmail = new Label("Email:");
+        txfEmail = new TextField();
+        pane.add(lblEmail,0, 3);
+        pane.add(txfEmail,1,3);
+
+        lblAdresse = new Label("Adresse:");
+        txfAdresse = new TextField();
+        pane.add(lblAdresse,0,4);
+        pane.add(txfAdresse,1,4);
+
+        btnOpretKunde = new Button("Opret kunde");
+        pane.add(btnOpretKunde,1,5);
+        btnOpretKunde.setOnAction(event -> this.OpretKunde());
+
+        lblError = new Label();
+        pane.add(lblError,1,6);
 
 
     }
@@ -48,20 +70,23 @@ public class OpretKundeWindow extends Stage {
 
 
     //Metoder:
+
+    public void updateControl(){
+
+    }
     public void OpretKunde() {
         String kundeNavn = txfNavn.getText().trim();
-        String kundeEmail = txfEmail.getText().trim();
         String kundeTelefon = txfTelefon.getText().trim();
+        String kundeEmail = txfEmail.getText().trim();
         String kundeAdresse = txfAdresse.getText().trim();
         if (kundeNavn.length() < 1 || kundeEmail.length() < 1 || kundeTelefon.length() < 1 || kundeAdresse.length() < 1) {
-            lblError.setText("Nogle felter mangler at blive udfyldt");
+            lblError.setText("Udfyld alle felter");
         } else {
             Kunde kunde = Controller.createKunde(kundeNavn, kundeTelefon, kundeEmail, kundeAdresse);
             lblError.setStyle("-fx-text-fill: green");
             lblError.setText("Kunde oprettet");
-
+            this.hide();
 
         }
-
     }
 }
